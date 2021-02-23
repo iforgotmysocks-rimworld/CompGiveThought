132b852ade5238d12dcf8b37a5a0142f0b2fa6b3
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2021-02-23 22:07:27 +0100

    created comp

---
+++ README.md
@@ -0,0 +1,4 @@
+# CompGiveThought
+
+Simple comp that can be assigned to new ThingDefs or added to existing ones, which takes a thought and some configuration as parameters. 
+Examples can be found in Patches\Patch.xml. An example thought can be found in Defs\ExampleThought.xml
---
+++ Source/CompGiveThought/CompGiveThought/CompProperties_GiveThought.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CompGiveThought
+{
+    public class CompProperties_GiveThought : CompProperties
+    {
+        public CompProperties_GiveThought()
+        {
+            this.compClass = typeof(CompGiveThought);
+        }
+
+        public ThoughtDef thoughtDef;
+        public int radius = 0;
+        public bool enableInInventory = false;
+    }
+}
